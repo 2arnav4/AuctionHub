061407199e24c5cdac4e9329dddcccc4e6b6fca1
2arnav4 <singlaarnav2405@gmail.com>
2026-07-10 18:33:41 +0530

    Calling of AuthInitializer in app.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,13 @@
 import { AppProviders } from "./providers";
 import { AppRouter } from "./router";
+import { AuthInitializer } from "./AuthInitializer";
 
 export function App() {
   return (
     <AppProviders>
-      <AppRouter />
+      <AuthInitializer>
+        <AppRouter />
+      </AuthInitializer>
     </AppProviders>
   );
 }
